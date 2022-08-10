cd29bc9ab1c8990246e47db814345890b404e84a
Andre Guimaraes <andrecpg@gmail.com>
2022-08-10 11:05:14 -0300

    WIP parser and builder

--- ts/src/parser.ts
+++ ts/src/parser.ts
@@ -1,14 +1,58 @@
-export function add(numbers: string): number {
-    let integers = numbers.split(',').map(x => parseInt(x));
-    let negatives = integers.filter(x => x < 0);
+import * as fs from 'fs'
 
-    if (negatives.length > 0)
-        throw new RangeError('Negatives are not allowed: ' + negatives.join(', '));
+export function parser(params: any) {
+    //const log = fs.readFileSync('quakelog/qgames.log', 'utf8')
+    const log = fs.readFileSync('quakelog/teste.log', 'utf8')
+    const games = log.split('InitGame')
+    const gamesData = []
+    games.shift()
+    let gamesCount = 0
+    
+    games.forEach(game => {
+        gamesCount++
+        let totalKills = 0
+        const players = new Set()
 
-    return integers
-        .filter(x => x <= 1000)
-        .reduce((a, b) => a + b, 0);
+        const killCount = game.match(/.Kill./gm)
+        const result = game.split(/\r?\n/)
+        const filtered = result.filter(value => {
+            return value.includes('killed')
+    })
+        console.log(filtered)
+
+        try {
+            totalKills = killCount.length
+            const kills = (game.match(/\d: (.)+killed (.)+ by/gm))
+            kills.forEach(kill => {
+                const killer = (kill.split(' '))[1]
+                const killed = (kill.split(' '))[3]
+                players.add(killer).add(killed)
+            })
+        } catch (error) {
+            totalKills = 0
+        }
+
+        const gameData = {
+            gamesCount,
+            totalKills,
+            players,
+            kills: {}
+        }
+        gamesData.push(gameData)
+    })
+    console.log(gamesData)
 }
 
-let result = add('1, 2, 4, 5');
-console.log(result);
+export function gameDataBuilder(gameCount: string, totalKills: number, playersSet: Set<string>, kills:object) {
+    const players = [...playersSet]
+    const gameData = {
+
+        gameCount: {
+            total_kills: totalKills,
+            players,
+            kills
+        }
+    }
+    return gameData
+}
+parser('lol')
